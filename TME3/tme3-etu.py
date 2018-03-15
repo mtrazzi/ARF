8a3c74a6ba1b298b4b9e748695c4b687cc160074
Julien Denes <julien50@sfr.fr>
2018-03-15 17:51:21 +0100

    Worked on tme3

--- TME3/tme3-etu.py
+++ TME3/tme3-etu.py
@@ -33,14 +33,14 @@ def load_usps(filename):
 
 ########################################
 
-def optimize(fonc, dfonc, x_init, eps, max_iter):
-    x_histo, f_histo, grad_histo = [x_init], [fonc(x_init)], [dfonc(x_init)]
+def optimize(fonc, dfonc, x_init, eps, max_iter=20):
+    x_histo, f_histo, grad_histo = x_init, fonc(x_init), dfonc(x_init)
     x = x_init
     for i in range(max_iter):
-        x = np.add(x,-eps * dfonc(x))
-        x_histo.append(x)
-        f_histo.append(fonc(x))
-        grad_histo.append(dfonc(x))
+        x = x - eps * dfonc(x)
+        x_histo = np.vstack((x_histo, x))
+        f_histo = np.vstack((f_histo, fonc(x)))
+        grad_histo = np.vstack((grad_histo, dfonc(x)))
     return x_histo, f_histo, grad_histo
 
 ########################################
@@ -49,27 +49,25 @@ def optimize(fonc, dfonc, x_init, eps, max_iter):
 
 ########################################
 
-from math import cos,log,sin
-
 def f1(x):
     return x * np.cos(x)
 
-def f2(x):https://github.com/mtrazzi/ARF.git
-    return -log(x) + x**2
+def f2(x):
+    return - np.log(x) + x**2
 
 def f3(x):
-	return 100*(x[:,1] - x[:,0]**2)**2 + (1 - x[:,0])**2
+    return 100*(x[:,1] - x[:,0]**2)**2 + (1 - x[:,0])**2
 
 def df1(x):
-	return cos(x) - x * sin(x)
+	return np.cos(x) - x * np.sin(x)
 
 def df2(x):
 	return -1/x + 2 * x
 
 def df3(x):
-	if (len(x.shape == 1)):
-		x = np.expand_dims(x, axis=0)
-	return np.array([200 * (-2*x[:,0]) * (x[:,1] - x[:,0]**2) + (-2) * (1 - x[:,0]), 100 * (x[:,1] - x[:,0]**2)])
+    return np.array([200 * (-2*x[:,0]) * (x[:,1] - x[:,0]**2) + (-2) * (1 - x[:,0]), 100 * (x[:,1] - x[:,0]**2)]).reshape(1,2)
+
+########################################
 
 def print2d(myfunction):
     grid,xx,yy = make_grid(-1,3,-1,3,20)
@@ -83,20 +81,6 @@ def print2d(myfunction):
     fig.colorbar(surf)
     plt.show()
 
-def grad_app(f,eps):
-    return lambda x : ((f(x + eps) - f(x - eps)) / (2 * eps))
-
-######
-
-# TEST
-
-######
-#print(optimize(f1, df1, 0, .005, 50))
-#print(optimize(f2, df2, 5, .005, 50))
-#print(optimize(f3, df3, np.array([8, 12]), .005, 50))
-
-#print2d(f3)
-
 def print_nb_iter(f, df, x_init, eps, max_iter):
 	x_histo, f_histo, grad_histo = optimize(f, df, x_init, eps, max_iter)
 	plt.figure()
@@ -105,20 +89,7 @@ def print_nb_iter(f, df, x_init, eps, max_iter):
 	plt.legend()
 	plt.show()	
 
-
-#print_nb_iter(f1, df1, 0, 0.1, 100)
-#print_nb_iter(f2, df2, 5, 0.1, 100)
-
 def print3d(f, df, x_init, eps, max_iter):
-	fig = plt.figure()
-	x_histo, f_histo, grad_histo = optimize(f, df, x_init, eps, max_iter)
-	ax = fig.add_subplot(111, projection='3d')
-	ax.plot(x_histo[:,0],x_histo[:,1], f_histo.ravel())
-	ax.plot(x_histo[:,0],x_histo[:,1], grad_histo.ravel())
-	plt.legend()
-	plt.show()
-
-def print3d_bis(f, df, x_init, eps, max_iter):
     grid,xx,yy = make_grid(-1,1,-1,1)
 	#print(np.shape(grid))
     plt.contourf(xx,yy,f(grid).reshape(xx.shape))
@@ -130,61 +101,35 @@ def print3d_bis(f, df, x_init, eps, max_iter):
     x_histo, f_histo, grad_histo = optimize(f, df, x_init, eps, max_iter)
     ax.plot(x_histo[:,0], x_histo[:,1], f_histo.ravel(), color='black')
     plt.show()
-###
-# RESIGN 3D
-###
-
-print3d_bis(f3, df3, np.array([[3,5]]).reshape(2,1), 0.1, 100)
-
 
 def print_f_and_approx(f, df, x_init, eps, max_iter):
-	x_histo, f_histo, grad_histo = optimize(f, df, x_init, eps, max_iter)
-	plt.figure()
-	plt.scatter(x_histo, f_histo, label="f", color='red')
-	plt.plot(x_histo, [f(x) for x in x_histo], label="f(x_i)")
-	plt.legend()
-	plt.show()	
-
-print_f_and_approx(f1, df1, 0, 0.1, 100)
+    x_histo, f_histo, grad_histo = optimize(f, df, x_init, eps, max_iter)
+    plt.figure()
+    plt.scatter(x_histo, f_histo, label="f", color='red')
+    plt.plot(x_histo, [f(x) for x in x_histo], label="f(x_i)")
+    plt.legend()
+    plt.show()	
+    print_f_and_approx(f1, df1, 0, 0.1, 100)
     return np.array(x_histo), np.array(f_histo), np.array(grad_histo)
 
 ########################################
 
-# OPTIMISATION DE FONCTIONS
+# MAIN
 
 ########################################
 
-from math import cos,log
+# Pour vérifier le bon fonctionnement de optimize
+"""
+print(optimize(f1, df1, 0, .005, 50))
+print("*"*30)
+print(optimize(f2, df2, 5, .005, 50))
+print("*"*30)
+print(optimize(f3, df3, np.array([[1, 2]]), .0005, 50))
+"""
 
-def f1(x):
-    return x * cos(x)
+# Affichage de f et df en fonction du nombre d'itérations
+print_nb_iter(f1, df1, 5, 0.1, 20)
+print_nb_iter(f2, df2, 5, 0.1, 20)
 
-def f2(x):
-    return -log(x) + x**2
-
-def f3(x):
-    return 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
-
-def print2d(myfunction):
-    grid,xx,yy = make_grid(-1,3,-1,3,20)
-    plt.figure()
-    plt.contourf(xx,yy,myfunction(grid).reshape(xx.shape))
-    
-    
-    
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(xx, yy, myfunction(grid).reshape(xx.shape),rstride=1,cstride=1,\
-    	  cmap=cm.gist_rainbow, linewidth=0, antialiased=False)
-    fig.colorbar(surf)
-    plt.show()
-
-#print2d(f1)
-
-def grad_app(f,eps):
-    return lambda x : ((f(x + eps) - f(x - eps)) / (2 * eps))
-
-print(optimize(f1, grad_app(f1, .001), 0, .005, 50))
-print(optimize(f2, grad_app(f1, .001), 5, .005, 50))
-print(optimize(f3, grad_app(f1, .001), np.array([8, 12]), .005, 50))
-
+# Affichage 3D de f3
+print3d(f3, df3, np.array([0,0]).reshape(1,2), 0.001, 20)
