4506e2d69142bbf5e2bb581c6619d28a76e64d09
Michaël Trazzi <mtrazzi@student.42.fr>
2018-03-31 20:34:25 +0200

    Fixed printing

--- TME2/tme2.py
+++ TME2/tme2.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pickle
 
-
-plt.ion()
+plt.interactive(False)
 parismap = mpimg.imread('data/paris-48.806-2.23--48.916-2.48.jpg')
 
 ## coordonnees GPS de la carte
@@ -13,11 +12,12 @@ ymin,ymax = 48.806,48.916 ## coord_y min et max
 
 def show_map():
     plt.imshow(parismap,extent=[xmin,xmax,ymin,ymax],aspect=1.5)
+    # plt.show()
     # extent pour controler l'echelle du plan
 
 poidata = pickle.load(open("data/poi-paris.pkl","rb"))
 ## liste des types de point of interest (poi)
-print("Liste des types de POI" , ", ".join(poidata.keys()))
+## print("Liste des types de POI" , ", ".join(poidata.keys()))
 
 ## Choix d'un poi
 typepoi = "night_club"
@@ -40,13 +40,6 @@ steps = 100
 xx,yy = np.meshgrid(np.linspace(xmin,xmax,steps),np.linspace(ymin,ymax,steps))
 grid = np.c_[xx.ravel(),yy.ravel()]
 
-
-
-
-
-
-
-
 ####################################################
 
 # HISTOGRAMMES-
@@ -60,8 +53,7 @@ show_map()
 plt.imshow(res,extent=[xmin,xmax,ymin,ymax],interpolation='none',\
                alpha=0.3,origin = "lower")
 plt.colorbar()
-
-
+plt.show()
 
 
 ####################################################
@@ -69,45 +61,44 @@ plt.colorbar()
 # METHODE DES NOYAUX-
 
 ####################################################
-#count = 0
-#def parzen(x, y, h):
-#    global geo_mat
-#    global count
-#    count += 1
-#    if ((count % 10000) == 0):
-#        print(count)
-#    s = 0
-#    for i in range(len(geo_mat)):
-#        (x_i,y_i) = geo_mat[i]
-#        if (abs(x-x_i) < h/2 and abs(y-y_i) < h/2):
-#            s += 1       
-#    return (s / (len(geo_mat) * h))
-#
-#parzen(48.3845, 2.35,2)
-#
-#nb_pix_x, nb_pix_y, _ = np.shape(parismap)
-#xedges = np.linspace(xmin, xmax, nb_pix_x // 100)
-#yedges = np.linspace(ymin, ymax, nb_pix_y // 100)
-#
-#def kernel_method(kernel, h):
-#    global nb_pix_x, nb_pix_y, xedges, yedges
-#    mat = np.zeros((nb_pix_x // 100, nb_pix_y // 100))
-#    for x in range(nb_pix_x // 100):
-#        for y in (range(nb_pix_y // 100)):
-#            mat[x,y] = kernel(yedges[y], xedges[x], h)
-#            print(yedges[y], xedges[x], h, mat[x,y])
-#    return mat
-#
-#res = kernel_method(parzen,0.01)
-#plt.figure()
-#show_map()
-#plt.imshow(res,extent=[xmin,xmax,ymin,ymax],interpolation='none',\
-#               alpha=0.3,origin = "lower")
-##imgplot = plt.imshow(mat)
-#plt.colorbar()
-#
-#plt.show()
-#
+count = 0
+def parzen(x, y, h):
+   global geo_mat
+   global count
+   count += 1
+##   if ((count % 10000) == 0):
+##      print(count)
+   s = 0
+   for i in range(len(geo_mat)):
+       (x_i,y_i) = geo_mat[i]
+       if (abs(x-x_i) < h/2 and abs(y-y_i) < h/2):
+           s += 1
+   return (s / (len(geo_mat) * h))
+
+parzen(48.3845, 2.35,2)
+
+nb_pix_x, nb_pix_y, _ = np.shape(parismap)
+xedges = np.linspace(xmin, xmax, nb_pix_x // 100)
+yedges = np.linspace(ymin, ymax, nb_pix_y // 100)
+
+def kernel_method(kernel, h):
+   global nb_pix_x, nb_pix_y, xedges, yedges
+   mat = np.zeros((nb_pix_x // 100, nb_pix_y // 100))
+   for x in range(nb_pix_x // 100):
+       for y in (range(nb_pix_y // 100)):
+           mat[x,y] = kernel(yedges[y], xedges[x], h)
+##         print(yedges[y], xedges[x], h, mat[x,y])
+   return mat
+
+res = kernel_method(parzen,0.01)
+plt.figure()
+show_map()
+plt.imshow(res,extent=[xmin,xmax,ymin,ymax],interpolation='none',\
+              alpha=0.3,origin = "lower")
+plt.colorbar()
+
+plt.show()
+
 ####################################################
 
 # discretisation pour la methode des histogrammes ?
